dfea3daf5ad0e3650416a95e912f1264d828f6bf
Critical-plr <grant.fitzgerald02@gmail.com>
2025-10-30 17:08:45 -0400

    Whelp. Here we go.

--- MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,8 +18,15 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public void onButtonPress()
+    {
+
+        setMenuVisibility(false);
+
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
@@ -30,7 +39,8 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onDestroyView() {
+    public void onDestroyView()
+    {
         super.onDestroyView();
         binding = null;
     }
